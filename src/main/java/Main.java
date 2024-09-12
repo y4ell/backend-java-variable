4f0166847b03a3f9764fac78649e4af8f5b46b89
y4ell <yaael.s@gmail.com>
2024-09-12 10:54:41 +0200

    fini

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,12 @@ public class Main {
     public static void main(String[] args) {
 
         String string_voorbeeld = "Dit is een voorbeeld";
-
-
-
-
-
+        String string_hello_world = "Hello world";
+        boolean boolean_true = true;
+        int int_four = 4;
+        short short_minus_eight = -8;
+        float float_six_point_five = 6.5F;
+        double double_minus_two_point_three = -2.3d;
         // Wijzig niets aan onderstaande System.out.println statements
 
         System.out.println(string_voorbeeld); // String Dit is een voorbeeld
@@ -19,6 +20,8 @@ public class Main {
         System.out.println(double_minus_two_point_three); // double -2.3
 
         // Bonus: Wijs een nieuwe waarde toe aan een bestaande variabele
+
+        string_voorbeeld = "nieuw voorbeeld";
 
         System.out.println(string_voorbeeld); // String Dit is een aangepast voorbeeld
     }
